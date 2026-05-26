aedc10906f6649f520d836484b45913eb9eb33fd
Hemansh Thakur <hemanshsinghthakur@gmail.com>
2026-05-27 01:01:07 +0530

    Updated model

--- ai_engine.py
+++ ai_engine.py
@@ -35,7 +35,7 @@ def analyze_ticket(issue):
     try:
 
         response = client.models.generate_content(
-            model="gemini-3.5-flash",
+            model="gemini-3.1-flash-lite",
             contents=prompt
         )
 
